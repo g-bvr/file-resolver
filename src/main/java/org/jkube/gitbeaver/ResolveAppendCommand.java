9003a989ebbd7972241290220855d3fa85678a90
jakneissler <jan.kneissler@breuninger.de>
2023-06-06 06:39:40 +0200

    Resolve and append command

--- src/main/java/org/jkube/gitbeaver/ResolveAppendCommand.java
+++ src/main/java/org/jkube/gitbeaver/ResolveAppendCommand.java
@@ -3,36 +3,34 @@ package org.jkube.gitbeaver;
 import org.jkube.gitbeaver.richfile.RichFile;
 import org.jkube.gitbeaver.util.FileUtil;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
 import static org.jkube.logging.Log.log;
-import static org.jkube.logging.Log.onException;
 
 /**
  * Usage: resolve source target
  */
-public class ResolveCommand extends AbstractCommand {
+public class ResolveAppendCommand extends AbstractCommand {
 
     private static final String SOURCE = "source";
     private static final String TARGET = "target";
 
-    public ResolveCommand() {
+    public ResolveAppendCommand() {
         super("resolve a file (using the rich file resolver)");
-        commandline("RESOLVE "+SOURCE+" TO "+TARGET);
+        commandline("RESOLVE "+SOURCE+" AND APPEND TO "+TARGET);
         argument(SOURCE, "the file to be resolved");
-        argument(TARGET, "the file into which the resolution result is written");
+        argument(TARGET, "the file to which the resolution result will be appended");
     }
 
     @Override
     public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
         Path sourcePath = workSpace.getAbsolutePath(arguments.get(SOURCE));
         Path targetPath = workSpace.getAbsolutePath(arguments.get(TARGET));
-        log("Resolving "+sourcePath+" to "+targetPath);
+        log("Resolving "+sourcePath+" and appending to "+targetPath);
         List<String> resolved = new RichFile(workSpace.getWorkdir(), sourcePath).resolve(variables);
         FileUtil.createIfNotExists(targetPath.getParent());
-        onException(() -> Files.write(targetPath, resolved)).fail("Could not write resolved lines to "+targetPath);
+        FileUtil.append(resolved, targetPath.toFile());
     }
 }
--- src/main/java/org/jkube/gitbeaver/ResolverPlugin.java
+++ src/main/java/org/jkube/gitbeaver/ResolverPlugin.java
@@ -5,7 +5,9 @@ import org.jkube.gitbeaver.plugin.SimplePlugin;
 public class ResolverPlugin extends SimplePlugin {
 
     public ResolverPlugin() {
-        super("Resolves text files by substituting variables, expanding macros and evaluate conditional sections", ResolveCommand.class);
+        super("Resolves text files by substituting variables, expanding macros and evaluate conditional sections",
+                ResolveAppendCommand.class,
+                ResolveCommand.class);
     }
     @Override
     public void init() {
