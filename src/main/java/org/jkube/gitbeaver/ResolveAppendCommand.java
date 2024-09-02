d8b0b0bdd1023f51f9c2f87505697b1ed95513f5
Jan Kneissler <jan.kneissler@breuninger.de>
2024-09-02 15:17:09 +0200

    removed dependency to java base, added files from java base, removed parent pom, added various plugins and properties from previous parent pom.

--- src/main/java/org/jkube/gitbeaver/ResolveAppendCommand.java
+++ src/main/java/org/jkube/gitbeaver/ResolveAppendCommand.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
-import static org.jkube.logging.Log.log;
+import static org.jkube.gitbeaver.logging.Log.log;
 
 /**
  * Usage: resolve source target
